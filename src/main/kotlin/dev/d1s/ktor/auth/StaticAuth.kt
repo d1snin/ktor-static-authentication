172741a443e62acbb6ece4e27958ac898ce4ca63
d1snin <me@d1s.dev>
2022-10-04 16:37:24 +0300

    adjusted the license

--- src/main/kotlin/dev/d1s/ktor/auth/StaticAuth.kt
+++ src/main/kotlin/dev/d1s/ktor/auth/StaticAuth.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Mikhail Titov and other contributors
+ * Copyright 2022 Mikhail Titov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
